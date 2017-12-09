0460217b6300774cc2acfac6c1cf13e499ce1c53
Eloy <me@eloy.codes>
2017-12-10 00:08:50 +0100

    feat: add Logo component

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './Logo.module.css'
+
+export default () => (
+  <svg
+    aria-labelledby='eloy-logo'
+    className={styles.e}
+    role='img'
+    viewBox='0 0 228 228'
+    xmlns='http://www.w3.org/2000/svg'>
+    <title id='eloy-logo'>Eloy Pineda logo</title>
+    <path
+      d='M114 228C51 228 0 177 0 114 0 51 51 0 114 0 177 0 228 51 228 114 228 177 177 228 114 228ZM171 95L171 57C171 46.5 162.5 38 152 38L76 38C65.5 38 57 46.5 57 57L57 171C57 181.5 65.5 190 76 190L133 190C143.5 190 152 181.5 152 171 152 160.5 143.5 152 133 152L114 152C103.5 152 95 143.5 95 133 95 122.5 103.5 114 114 114L152 114C162.5 114 171 105.5 171 95ZM95 76C95 65.5 103.5 57 114 57 124.5 57 133 65.5 133 76 133 86.5 124.5 95 114 95 103.5 95 95 86.5 95 76Z'
+      fill='#009499' />
+  </svg>
+)
